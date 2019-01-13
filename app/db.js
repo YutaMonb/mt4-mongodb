b39f822026684e3c7669781e65974f67b222878f
yutamonb <yutamonb@gmail.com>
2019-01-14 04:10:01 +0900

    mongooseでいい感じに

---
+++ app/db.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let mongoose = require('mongoose')
+require('mongoose-double')(mongoose);
+
+mongoose.connect(process.env.DB_HOST, {useNewUrlParser: true});
+
+let Schema = mongoose.Schema;
+let SchemaTypes = Schema.Types;
+
+let Tick = new Schema({
+    symbol : {type: String, require: true}, // GBP_JPY
+    bid: {type: SchemaTypes.Double, require: true}, // 139.283
+    ask: {type: SchemaTypes.Double, requrie: true}, // 139.461
+    timestamp: {type: number, require: true}
+});
+
+
+module.exports = mongoose.model('tick', Tick);
