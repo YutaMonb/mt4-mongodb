bbc199b9af6ebb8c636ab04ea42bc182492e6036
yutamonb <yutamonb@gmail.com>
2019-01-14 04:43:55 +0900

    API叩いたらいい感じにMongoに突っ込むように

--- app/src/endpoint/tick.js
+++ app/src/endpoint/tick.js
@@ -2,9 +2,25 @@
 
 let express = require('express');
 let router = express.Router();
+let Tick = require('../db');
 
+// http://localhost:8088/tick?symbol=GBP_JPY&bid=139.283&ask=139.461&timestamp=1547407024
+router.get('/', (req, res) => {
+    let tick = new Tick();
+    tick.symbol = req.query.symbol;
+    tick.bid = req.query.bid;
+    tick.ask = req.query.ask;
+    tick.timestamp = req.query.timestamp;
 
-router.get('/', (req, res, next) => {
+    tick.save((err) => {
+        if (err) {
+            console.log(err);
+            res.statusCode(500);
+            return;
+        }
+        console.log('success');
+        res.sendStatus(200);
+    });
 
 });
 
